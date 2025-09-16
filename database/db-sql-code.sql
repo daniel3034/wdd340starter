5a2e834e0e36895c5f89fb992b4bf7eea6ef6ea3
daniel3034 <danielh3034@gmail.com>
2025-09-16 11:06:48 -0500

    assignment2

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -237,3 +237,11 @@ VALUES   (
     5
   );
 
+UPDATE inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
+    
