276c62bbb9fa6a4844fd1db7800fcb7331892fb7
daniel3034 <danielh3034@gmail.com>
2025-09-28 22:19:41 -0500

    Account: Deliver Login

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities/")
 const session = require("express-session")
 const pool = require('./database/')
+const accountRoute = require("./routes/accountRoute")
 
 // Set up view engine and layouts
 app.set("view engine", "ejs")
@@ -52,6 +53,9 @@ app.get("/", utilities.handleErrors(baseController.buildHome))
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
+
+// Account routes
+app.use("/account", accountRoute)
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
